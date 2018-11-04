431dbfc0f7fb3a835f29ea293855345747521357
Pranjal <pranjal@brave.com>
2018-11-04 00:17:36 -0700

    STP - First Iteration
    1. Parse the FirstPartyTracker list provided by the `tracking-protection` extension.
    2. On any request to Store data (Cookies, localStorage, sessionStorage, WebSQL, indexedDB)
      1. Check if the Shield Settings and tracker blocking is allowed for the site that initiated the redirects
      2. Check if the site is in the Storage trackers list.
    3. Deny if the URL is found in the tracker list.
    4. Increment the tracker count if its found.
    
    auditors: @bridiver

---
+++ components/brave_shields/browser/tracking_protection_helper.h
@@ -0,0 +1,59 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_SHIELDS_BROWSER_TRACKING_PROTECTION_HELPER_H_
+#define BRAVE_COMPONENTS_BRAVE_SHIELDS_BROWSER_TRACKING_PROTECTION_HELPER_H_
+
+#include "base/macros.h"
+#include "base/synchronization/lock.h"
+#include "base/strings/string16.h"
+#include "content/public/browser/web_contents_observer.h"
+#include "content/public/browser/web_contents_user_data.h"
+
+
+
+namespace content {
+class WebContents;
+class NavigationHandle;
+}
+
+class TrackingProtectionService;
+
+namespace brave_shields {
+
+class TrackingProtectionHelper : public content::WebContentsObserver,
+  public content::WebContentsUserData<TrackingProtectionHelper> {
+  public:
+    TrackingProtectionHelper(content::WebContents*);
+    ~TrackingProtectionHelper() override;
+    void DidStartNavigation(content::NavigationHandle* 
+      navigation_handle) override;
+    static GURL GetStartingSiteURLFromRenderFrameInfo(int render_process_id, 
+      int render_frame_id);
+
+  protected:
+    // A set of identifiers that uniquely identifies a RenderFrame.
+    struct RenderFrameIdKey {
+      RenderFrameIdKey();
+      RenderFrameIdKey(int render_process_id, int frame_routing_id);
+
+    // The process ID of the renderer that contains the RenderFrame.
+    int render_process_id;
+
+    // The routing ID of the RenderFrame.
+    int frame_routing_id;
+
+    bool operator<(const RenderFrameIdKey& other) const;
+    bool operator==(const RenderFrameIdKey& other) const;
+  };
+
+  static std::map<RenderFrameIdKey, GURL> render_frame_key_to_starting_site_url;
+  static base::Lock frame_data_map_lock_;
+
+  DISALLOW_COPY_AND_ASSIGN(TrackingProtectionHelper);
+};
+
+}  // namespace brave_shields
+
+#endif  // BRAVE_COMPONENTS_BRAVE_SHIELDS_BROWSER_TRACKING_PROTECTION_HELPER_H_
